95c45bf0622909fa32f4cd4171f12806896dc849
user01 <user01@training.local>
2026-04-20 23:42:00 +0900

    最初のコミット

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello():
     return 'Hello World! デプロイ成功！'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=52XX)  # XX を自分のポート番号に変更
+    app.run(host='0.0.0.0', port=5201)  # XX を自分のポート番号に変更
